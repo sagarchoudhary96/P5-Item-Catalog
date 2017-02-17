68d12c322411bd26e9319cdc78e4e6458e9d4908
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-02-17 18:42:09 +0530

    Add: client side validation for adding new book

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 $(".brighten").click(function() {
-  window.location = $(this).find("a").attr("href"); 
+  window.location = $(this).find("a").attr("href");
   return false;
 });
 
@@ -9,3 +9,61 @@ $(".demo-card-wide").hover(
 
     }
 );
+
+var notification = document.querySelector('.mdl-js-snackbar');
+
+var validateDetails = function() {
+  var bookname = $('#bookname');
+  var authorname = $('#authorname');
+  var coverUrl = $('#coverurl');
+  var description = $('#description');
+  var category = $('#category');
+
+  if (bookname.val() == "") {
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: "Book name Can't  Empty!"
+      }
+    );
+    bookname.focus();
+    return false;
+  }
+  else if (authorname.val() == "") {
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: "Author Name also required :)"
+      }
+    );
+    authorname.focus();
+    return false;
+  }
+  else if (coverUrl.val() == "") {
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: "Cover Image Url also Required!"
+      }
+    );
+    coverUrl.focus();
+    return false;
+  }
+  else if (description.val() == "") {
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: "Please provide some description of book!"
+      }
+    );
+    description.focus();
+    return false;
+  }
+  else if (category.val() == "") {
+    notification.MaterialSnackbar.showSnackbar(
+      {
+        message: "Please Select Book Genre!"
+      }
+    );
+    return false;
+  }
+
+  $('#newBook').submit();
+
+};
